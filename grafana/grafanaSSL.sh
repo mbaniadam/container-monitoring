f172c4bd73f8076ee9e623374d81b71be3835f9e
Morteza Baniadam <mortalbm@gmail.com>
2023-07-30 16:51:20 +0330

    fix permision

--- grafana/grafanaSSL.sh
+++ grafana/grafanaSSL.sh
@@ -2,7 +2,7 @@
 openssl genrsa -out grafana.key
 openssl req -new -key grafana.key -out grafana.csr
 openssl x509 -req -days 3650 -in grafana.csr -signkey grafana.key -out grafana.crt
-chown grafana:grafana grafana.crt
-chown grafana:grafana grafana.key
+#hown grafana:grafana grafana.crt
+#chown grafana:grafana grafana.key
 chmod 400 grafana.key 
 chmod 774 grafana.crt
