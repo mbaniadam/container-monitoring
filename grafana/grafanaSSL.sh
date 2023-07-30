b1ef724d3e043d2e3b5895e83bf6624c5209f0a2
Morteza Baniadam <mortalbm@gmail.com>
2023-07-30 13:14:12 +0330

    added bash script for ssl

--- grafana/grafanaSSL.sh
+++ grafana/grafanaSSL.sh
@@ -2,6 +2,3 @@
 openssl genrsa -out grafana.key
 openssl req -new -key grafana.key -out grafana.csr
 openssl x509 -req -days 3650 -in grafana.csr -signkey grafana.key -out grafana.crt
-chown grafana:grafana grafana.crt
-chown grafana:grafana grafana.key
-chmod 400 grafana.key grafana.crt
